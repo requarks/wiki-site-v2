a0e63f815506bf63970dc40d4ee99d52b19b83ed
Nicolas Giard <github@ngpixel.com>
2017-08-05 16:23:07 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -10,8 +10,7 @@ Write-Host "   < INSTALL >`n"
 
 $VERSION = "1.0.4"
 
-$invocation = (Get-Variable MyInvocation).Value
-$curPath = Split-Path $invocation.MyCommand.Path
+$curPath = Get-ChildItem -Path (Read-Host -Prompt 'Full path where Wiki.js should be installed?')
 $downloader = New-Object System.Net.WebClient
 $7zip = "$curPath\7za.exe"
 
