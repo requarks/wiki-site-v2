3407f6120679d64142c19926cec50b7c1b7ba026
Nicolas Giard <github@ngpixel.com>
2017-08-12 19:26:44 -0400

    feat: use online VERSION reference (linux)

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-VERSION=1.0.6
 bold=$(tput bold)
 green=$(tput setaf 2)
 normal=$(tput sgr0)
 
 echo -e "\n\n __    __ _ _    _    _     \n/ / /\\ \\ (_) | _(_)  (_)___ \n\\ \\/  \\/ / | |/ / |  | / __| \n \\  /\\  /| |   <| |_ | \\__ \\ \n  \\/  \\/ |_|_|\\_\\_(_)/ |___/ \n                   |__/\n   < INSTALL >\n"
+
+VERSION=$(curl -L -s -S https://wiki.js.org/VERSION)
 
 echo "[1/3] Fetching latest build..."
 curl -L -s -S https://github.com/Requarks/wiki/releases/download/v$VERSION/wiki-js.tar.gz | tar xz -C .
